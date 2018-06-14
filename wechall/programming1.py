8e9bcc32df79b98e579645017307b1b9250f5378
li.jun <li.jun@le-yao.com>
2018-06-14 16:11:55 +0800

    update

--- wechall/programming1.py
+++ wechall/programming1.py
@@ -1,9 +1,17 @@
 #!/bin/env python
 
-import requests
+import urllib2
+import webbrowser  
+headers = {'Cookie':'WC=10545137-41483-LE03v79NlZdHHH9H'}
+req = urllib2.Request('http://www.wechall.net/challenge/training/programming1/index.php?action=request',
+    headers=headers)
+f = urllib2.urlopen(req)
+res = f.read()
 
-cookie = dict(WC='10544425-41483-NoNoGwa1dTXATn0R')
-res = requests.get('http://www.wechall.net/challenge/training/programming1/index.php?action=request',
-    cookies=cookie)
+webbrowser.open('http://www.wechall.net/challenge/training/programming1/index.php?answer=' + res, new=0, autoraise=True)  
+print 'http://www.wechall.net/challenge/training/programming1/index.php?answer=' + res
+req2 = urllib2.Request('http://www.wechall.net/challenge/training/programming1/index.php?answer=' + res,
+    headers=headers)
+f2 = urllib2.urlopen(req)
+print f2.read()
 
-print res
